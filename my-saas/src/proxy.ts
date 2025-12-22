135189f9f47204e9a9585f7e4d4a1ab20db75c69
Diego Silva <diegos9583@gmail.com>
2025-12-22 01:25:27 -0800

    feat: add Clerk auth routes and protect /dasboard

---
+++ my-saas/src/proxy.ts
@@ -0,0 +1,19 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+// Public pages (anyone can access)
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/(en|es)"
+]);
+
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) await auth.protect()
+})
+
+export const config = {
+  // Run middleware on all routes except Next.js internals and static files
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
